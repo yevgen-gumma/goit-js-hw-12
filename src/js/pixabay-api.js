db5b246050b5885df62e3abc5857b99e3e9632ff
yevgen-gumma <203040eg@gmail.com>
2024-03-05 00:19:17 +0200

    4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,111 +1,23 @@
-import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import axios from 'axios';
-import { render } from './render-functions';
-
-const searchInput = document.querySelector('.searchInput');
-const loader = document.querySelector('.loader');
-const container = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.loadMoreBtn');
-
-let page = 1;
-let totalHits = 0;
 
 const KEY = '42324270-89622daef349524aeb531ebd1';
 const BASE_URL = 'https://pixabay.com/api/';
 const otherParams =
   'image_type=photo&orientation=horizontal&safesearch=true&per_page=15';
 
-export async function getImages() {
-  page = 1;
-
-  const query = searchInput.value.trim();
-  const LINK = `${BASE_URL}?key=${KEY}&q=${query}&${otherParams}`;
-
-  loader.classList.remove('hidden');
-  container.innerHTML = '';
-
-  try {
-    const response = await axios.get(LINK);
-    const data = response.data;
-
-    if (data.hits.length === 0) {
-      loadMoreBtn.classList.add('hidden');
-      iziToast.show({
-        position: 'topRight',
-        timeout: 3500,
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        close: false,
-        messageColor: 'white',
-        backgroundColor: 'green',
-      });
-    } else {
-      render(data.hits);
-      if (data.hits.length >= 15) {
-        loadMoreBtn.classList.remove('hidden');
-      } else {
-        loadMoreBtn.classList.add('hidden');
-      }
-    }
-  } catch (error) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Error while rendering images',
-      position: 'topRight',
-      timeout: 2500,
-    });
-  } finally {
-    loader.classList.add('hidden');
-  }
-}
-
-export async function loadMoreImages() {
-  page += 1;
-
-  const query = searchInput.value.trim();
+export async function fetchImages(query, page, iziToast) {
   const LINK = `${BASE_URL}?key=${KEY}&q=${query}&${otherParams}&page=${page}`;
 
-  let data;
-
   try {
     const response = await axios.get(LINK);
-    data = response.data;
-    totalHits = data.totalHits;
-
-    render(data.hits);
-
-    smoothScrollToGallery();
+    return response.data;
   } catch (error) {
     iziToast.error({
       title: 'Error',
-      message: 'Error while rendering images',
+      message: 'Error while fetching images',
       position: 'topRight',
       timeout: 2500,
     });
-  } finally {
-    if (data && page * 15 >= data.totalHits) {
-      loadMoreBtn.classList.add('hidden');
-
-      iziToast.show({
-        position: 'topRight',
-        timeout: 3500,
-        message: "We're sorry, but you've reached the end of search results.",
-        close: false,
-        messageColor: 'white',
-        backgroundColor: 'green',
-      });
-    } else {
-      loadMoreBtn.classList.remove('hidden');
-    }
   }
-}
-
-function smoothScrollToGallery() {
-  const cardHeight = container.firstElementChild.getBoundingClientRect().height;
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImages, loadMoreImages } from './js/pixabay-api';
+import { fetchImages } from './js/pixabay-api';
+import { render } from './js/render-functions';
 
 const submitForm = document.querySelector('.submitForm');
 const searchInput = document.querySelector('.searchInput');
 const loadMoreBtn = document.querySelector('.loadMoreBtn');
+const loader = document.querySelector('.loader');
+const container = document.querySelector('.gallery');
 
-submitForm.addEventListener('submit', event => {
+let page = 1;
+
+submitForm.addEventListener('submit', async event => {
   event.preventDefault();
 
   const query = searchInput.value.trim();
@@ -24,10 +29,92 @@ submitForm.addEventListener('submit', event => {
     return;
   }
 
-  getImages();
+  try {
+    loader.classList.remove('hidden');
+    container.innerHTML = '';
+    page = 1;
+
+    const data = await fetchImages(query, page, iziToast);
+
+    if (data.hits.length === 0) {
+      loadMoreBtn.classList.add('hidden');
+      iziToast.show({
+        position: 'topRight',
+        timeout: 3500,
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        close: false,
+        messageColor: 'white',
+        backgroundColor: 'green',
+      });
+    } else {
+      render(data.hits);
+      if (data.hits.length >= 15) {
+        loadMoreBtn.classList.remove('hidden');
+      } else {
+        loadMoreBtn.classList.add('hidden');
+      }
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Error while rendering images',
+      position: 'topRight',
+      timeout: 2500,
+    });
+  } finally {
+    loader.classList.add('hidden');
+  }
 });
 
-loadMoreBtn.addEventListener('click', () => {
+loadMoreBtn.addEventListener('click', async () => {
   loadMoreBtn.classList.remove('hidden');
-  loadMoreImages();
+
+  let data;
+  let totalHits;
+
+  page += 1;
+
+  const query = searchInput.value.trim();
+
+  try {
+    data = await fetchImages(query, page, iziToast);
+
+    totalHits = data.totalHits;
+
+    render(data.hits);
+
+    smoothScrollToGallery();
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Error while rendering images',
+      position: 'topRight',
+      timeout: 2500,
+    });
+  } finally {
+    if (data && page * 15 >= data.totalHits) {
+      loadMoreBtn.classList.add('hidden');
+
+      iziToast.show({
+        position: 'topRight',
+        timeout: 3500,
+        message: "We're sorry, but you've reached the end of search results.",
+        close: false,
+        messageColor: 'white',
+        backgroundColor: 'green',
+      });
+    } else {
+      loadMoreBtn.classList.remove('hidden');
+    }
+  }
 });
+
+function smoothScrollToGallery() {
+  const cardHeight = container.firstElementChild.getBoundingClientRect().height;
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
